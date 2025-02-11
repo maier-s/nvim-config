20e86d1540a7a62512306b9434de71084eaae4ed
Stefan Maier <Stefan.Maier95tt@web.de>
2025-02-11 13:40:12 +0100

    Added custom configuration for nvim config

---
+++ lua/custom/plugins.lua
@@ -0,0 +1,85 @@
+local plugins = {
+  {
+    "github/copilot.vim",
+    lazy = false,
+    config = function() -- Mapping tab is already used in NvChad
+      vim.g.copilot_no_tab_map = true; -- Disable tab mapping
+      vim.g.copilot_assume_mapped = true; -- Assume that the mapping is already done
+      vim.g.copilot_proxy_strict_ssl = false;
+    end
+  },
+  {
+    "lervag/vimtex",
+    lazy = false,
+    init = function ()
+      vim.g.vimtex_view_method = "general"
+      vim.g.maplocalleader = " "
+      vim.g.vimtex_compiler_method = "latexmk"
+    end
+  },
+  {
+    "rcarriga/nvim-dap-ui",
+    dependencies = {"mfussenegger/nvim-dap", "nvim-neotest/nvim-nio"},
+    config = function ()
+      local dap = require("dap")
+      local dapui = require("dapui")
+      dapui.setup()
+      dap.listeners.after.event_initialized["dapui_config"] = function ()
+        dapui.open()
+      end
+      dap.listeners.before.event_terminated["dapui_config"] = function()
+        dapui.close()
+      end
+      dap.listeners.before.event_exited["dapui_config"] = function ()
+        dapui.close()
+      end
+    end,
+  },
+  {
+    "mfussenegger/nvim-dap",
+    config = function (_,opts)
+      require("core.utils").load_mappings("dap")
+    end
+  },
+  {
+    "mfussenegger/nvim-dap-python",
+    ft = "python",
+    dependencies = {
+      "mfussenegger/nvim-dap",
+      "rcarriga/nvim-dap-ui",
+    },
+    config = function (_,opts)
+      local path = "~/AppData/Local/nvim-data/mason/packages/debugpy/venv/Scripts/python.exe"
+      require("dap-python").setup(path)
+      require("core.utils").load_mappings("dap_python")
+    end,
+  },
+  {
+    "jose-elias-alvarez/null-ls.nvim",
+    ft = {"python"},
+    opts = function ()
+      return require("custom.configs.null-ls")
+    end,
+  },
+  {
+    "williamboman/mason.nvim",
+    opts = {
+      ensure_installed = {
+        "black",
+        "debugpy",
+        "mypy",
+        "ruff",
+        "pyright",
+      },
+    },
+  },
+  {
+    "neovim/nvim-lspconfig",
+    config = function ()
+      require("plugins.configs.lspconfig")
+      require("custom.configs.lspconfig")
+    end,
+  }
+}
+
+return plugins
